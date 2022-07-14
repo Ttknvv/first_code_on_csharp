b3d50bb67d885a31f260744ed53e2b72318092a9
Vova <vvvova24@gamil.com>
2022-07-14 20:33:16 +0700

    Homework4

---
+++ Hw4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число =");
+double chisl = Convert.ToDouble(Console.ReadLine());
+int numb = 1;
+while(numb != chisl)
+{
+    if(numb%2 == 0)
+    {
+        Console.WriteLine(numb);
+    }
+    numb++;
+}
