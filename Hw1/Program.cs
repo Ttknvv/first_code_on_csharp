a761b4e2ecac0c887d7b31bf46ed2c62864ef5f1
Vova <vvvova24@gamil.com>
2022-07-14 19:53:36 +0700

    Homework

---
+++ Hw1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число а = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b) 
+{
+    Console.WriteLine("Max = ");
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.WriteLine("Max = ");
+    Console.WriteLine(b);
+}
---
+++ Hw2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число а = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if(b>max)
+{
+    max = b;
+}
+else if(c>max)
+{
+    max = c;
+}
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
