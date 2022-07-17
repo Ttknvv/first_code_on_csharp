62bafea23d35f2a2cb18748942126699fffa326b
Vova <vvvova24@gamil.com>
2022-07-17 16:57:03 +0700

    New hw2_1

--- Hw2_1/Program.cs
+++ Hw2_1/Program.cs
@@ -1,6 +1,21 @@
 ﻿Console.WriteLine("Введите число");
 int Num = Convert.ToInt32(Console.ReadLine());
+int numerik = Num;
 
+if(Num>99)
+{
+    while(numerik>999)
+    {
+        numerik = numerik/10;
+    }
+    Console.WriteLine(numerik%10);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
+/*
 if(Num/100 > 0)
 {
     Console.WriteLine(Convert.ToString(Num)[2]);
@@ -8,4 +23,4 @@ if(Num/100 > 0)
 else
 {
     Console.WriteLine("третьей цифры нет");
-}
+}*/
