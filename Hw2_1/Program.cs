03a6c7ed967f6f6af1dd5b808753d92f21021cdc
Vova <vvvova24@gamil.com>
2022-07-17 11:57:25 +0700

    HW2.1 and HW2.2

---
+++ Hw2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if(Num/100 > 0)
+{
+    Console.WriteLine(Convert.ToString(Num)[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
---
+++ Hw2_2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите день недели ");
+int Num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if(Num<6) {Console.WriteLine("Рабочий день");}
+else if(Num<8) {Console.WriteLine("Выходной");}
+else {Console.WriteLine("Не в промежутке");}
